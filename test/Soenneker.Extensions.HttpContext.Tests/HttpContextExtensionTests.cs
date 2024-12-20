08e81c7f807229e58b5515c3318182d974c0ba09
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:20 +0000

    Tests

---
+++ test/Soenneker.Extensions.HttpContext.Tests/HttpContextExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.HttpContext.Tests;
+
+public class HttpContextExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
